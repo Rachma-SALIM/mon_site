945b1920140d1b24efc8d7eb3d9974459336dd83
Rachma SALIM <rachma.salim.career@hotmail.fr>
2018-05-18 10:28:12 +0200

    ajout paragraphe + css

--- index.php
+++ index.php
@@ -24,10 +24,18 @@
     } 
 
     p {
-        font-size: 3rem;
+        font-size: 1rem;
         text-align: center;
     }
     p:hover {color: red;}
+
+    .un:hover{
+        color: darkgrey;
+    }
+
+    .deux:hover{
+        color: fuchsia;
+    }
 
 </style>
 </head>
@@ -37,6 +45,12 @@
         <h1> Welcome to our website</h1>
 
         <p>Isn't this the best website ever??? </p>
+
+        <p class="un">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo dignissimos, doloribus eligendi, laudantium magnam eius, voluptatum dolorum asperiores vitae tempore sequi illum modi? Veniam unde voluptatem architecto corrupti atque! <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus itaque similique exercitationem aperiam magnam ipsa libero, ea, accusantium magni dignissimos, quaerat quisquam consectetur quas placeat ut nobis incidunt amet iste velit laboriosam. Ab, consequatur neque debitis aut eveniet nostrum repellat voluptatibus maiores illo, soluta, dolorum nisi nesciunt perferendis quaerat. Quae, dignissimos. Ea possimus temporibus eligendi facilis, voluptatum itaque architecto tenetur numquam! Totam cum doloremque quis dolorem aut officiis et excepturi distinctio culpa cupiditate quas quos, eos eaque mollitia id </p>
+        <p class="deux">
+        inventore nesciunt unde dolores! Nesciunt, ipsum! Maiores aspernatur fuga voluptates consectetur a beatae, officiis in, rem eos impedit sapiente accusamus modi, illo ullam tempore quod quaerat sit? Harum, dolores sint error, rem molestias quia exercitationem asperiores natus, voluptate saepe voluptas eum a debitis accusantium. Ipsam deserunt corporis magni exercitationem libero delectus optio ex quasi assumenda in debitis voluptatem, inventore temporibus aut fugit architecto, culpa quisquam. Cupiditate esse quibusdam amet fugiat! Magnam dignissimos repellat id ipsum, unde perspiciatis aspernatur ut quis possimus similique iure illum sed minus nesciunt! Voluptas suscipit ipsam sit officia architecto dolorum enim sunt quo! Ipsum dolores deleniti nisi consequatur voluptatum aut eaque, enim itaque praesentium cupiditate reprehenderit optio sunt ut provident! Facilis, dolores. Minima accusamus soluta dolore alias quia rerum atque ut numquam debitis quod reprehenderit laudantium commodi nemo recusandae voluptatum dolorem cumque minus quasi, doloremque laborum quidem accusantium voluptate nostrum! Dolorum blanditiis excepturi explicabo, in fuga cumque quae, voluptate repellat quasi eos atque vitae nulla dolores quaerat laborum ipsum eveniet voluptatibus id praesentium veniam quidem ut laudantium eligendi necessitatibus! Dolores praesentium enim et doloribus similique impedit maxime iste. Quis illum corporis sit repudiandae voluptatem! Voluptatibus inventore deserunt vitae necessitatibus at! Incidunt esse numquam ex adipisci dolor error aliquam minus labore qui laboriosam perspiciatis tenetur quod, quae minima corporis voluptas aliquid nemo, quo modi iste atque? Quos!
+        </p>
 
      </div> 
 </body>
