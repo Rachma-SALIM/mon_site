d7dbc5041193ddd70cf2a86aa76046f5b7fe771b
Rachma SALIM <rachma.salim.career@hotmail.fr>
2018-05-18 10:06:45 +0200

    ajout css

--- index.php
+++ index.php
@@ -5,9 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mon Site</title>
+    <style>
+    body {
+    background-color: linen;
+    padding: 5rem;
+    }
+
+    .web {
+        padding-left: 10rem;
+        padding-right: 10rem;
+        background-color: white;
+    }
+
+    h1 {
+    color: maroon;
+    margin-left: 40px;
+    text-align: center;
+    } 
+
+    p {
+        font-size: 3rem;
+        text-align: center;
+    }
+    p:hover {color: red;}
+
+</style>
 </head>
 
 <body>
-    <p>Hello!!!</p>
+    <div class="web">
+        <h1> Welcome to our website</h1>
+
+        <p>Isn't this the best website ever??? </p>
+
+     </div> 
 </body>
 </html>
